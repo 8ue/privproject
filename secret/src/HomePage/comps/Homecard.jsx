0f10d9b2273b37314f4fc132003f5e6e0f5e2afc
James <2jcjl4@gmail.com>
2022-06-21 17:30:39 +0100

    testing homepage

---
+++ secret/src/HomePage/comps/Homecard.jsx
@@ -0,0 +1,9 @@
+import stretchCinema from '../../Media/HomePageMed/stretchCinema';
+
+function Homecard() {
+    return(
+        <>
+         <img src={stretchCinema} alt={PictureOfCinema} width="350px"/>
+        </>
+    );
+} 
